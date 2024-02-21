236bd61b57fab5c64a74d5137f0d1a64d314ac67
rgz-felipe <77169098+rgz-Felipe@users.noreply.github.com>
2024-02-21 01:00:17 -0300

    acrescentando variaveis na Classe Aluno

--- EM.Domain/Aluno.cs
+++ EM.Domain/Aluno.cs
@@ -8,5 +8,40 @@ namespace EM.Domain
 {
     internal class Aluno
     {
-    }
+
+        private int matricula;
+        private string nome;
+        private string cpf;
+        private DateTime nascimento;
+        private EnumeradorSexo sexo;
+
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string CPF
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        public DateTime Nascimento
+        {
+            get { return nascimento; }
+            set { nascimento = value; }
+        }
+
+        
+
+
+        }
 }
